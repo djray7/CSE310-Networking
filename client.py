9be5ac7fd21ba9279414da2ae78360404dc8a611
djray7 <55644676+djray7@users.noreply.github.com>
2022-07-22 00:14:58 -0600

    client upload

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+SERVER = "172.17.240.1"
+# SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT) 
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!Disconnect!"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World")
